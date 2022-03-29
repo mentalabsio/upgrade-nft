1b3cd725c997fbb059f09969b7bfbd9035e2dd78
lucas <lucasfonsecard@icloud.com>
2022-03-29 06:51:58 -0300

    feat: create program with `upgrade` handler

---
+++ programs/upgrade-nft/src/lib.rs
@@ -0,0 +1,100 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
+use anchor_spl::token::{Mint, Token, TokenAccount};
+use solutils::{
+    charge::*,
+    wrappers::metadata::{MetadataAccount, TokenMetadata, UpdateMetadataAccountV2},
+};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod upgrade_nft {
+    use solutils::mpl_token_metadata::state::{Data, DataV2, Metadata};
+    use solutils::wrappers::metadata;
+
+    use super::*;
+
+    #[access_control(token_fee(&ctx, fee))]
+    pub fn upgrade(ctx: Context<Upgrade>, fee: u64, new_uri: String) -> Result<()> {
+        let Metadata {
+            data:
+                Data {
+                    name,
+                    symbol,
+                    creators,
+                    seller_fee_basis_points,
+                    ..
+                },
+            uses,
+            collection,
+            ..
+        } = (&**ctx.accounts.token_metadata).clone();
+
+        let data = DataV2 {
+            uri: new_uri,
+            name,
+            symbol,
+            seller_fee_basis_points,
+            creators,
+            uses,
+            collection,
+        };
+
+        metadata::update_metadata_accounts_v2((&*ctx.accounts).into(), data)
+    }
+}
+
+#[derive(Accounts, Chargeable)]
+pub struct Upgrade<'info> {
+    pub mint_address: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        constraint = token_metadata.is_mutable,
+        constraint = token_metadata.mint == mint_address.key(),
+        constraint = token_metadata.update_authority == update_authority.key()
+    )]
+    pub token_metadata: Account<'info, MetadataAccount>,
+
+    #[account(mut)]
+    pub update_authority: Signer<'info>,
+
+    #[fee_payer]
+    pub user_account: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = user_token_account.mint == mint_address.key(),
+        constraint = user_token_account.owner == user_account.key()
+    )]
+    pub user_token_account: Account<'info, TokenAccount>,
+
+    #[account(address = solana_program::incinerator::ID)]
+    pub incinerator: SystemAccount<'info>,
+
+    #[account(mut)]
+    pub fee_payer_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = fee_incinerator_ata.owner == incinerator.key()
+    )]
+    pub fee_incinerator_ata: Box<Account<'info, TokenAccount>>,
+
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, TokenMetadata>,
+}
+
+impl<'info> From<&Upgrade<'info>>
+    for CpiContext<'_, '_, '_, 'info, UpdateMetadataAccountV2<'info>>
+{
+    fn from(ctx: &Upgrade<'info>) -> Self {
+        let accounts = UpdateMetadataAccountV2 {
+            metadata_account: ctx.token_metadata.to_account_info(),
+            update_authority: ctx.update_authority.to_account_info(),
+        };
+
+        CpiContext::new(ctx.token_metadata_program.to_account_info(), accounts)
+    }
+}
---
+++ tests/upgrade-nft.ts
@@ -0,0 +1,88 @@
+import {
+  MetadataData,
+  MetadataProgram,
+} from "@metaplex-foundation/mpl-token-metadata";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import assert from "assert";
+
+import { UpgradeNft } from "../target/types/upgrade_nft";
+
+describe("upgrade-nft", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.UpgradeNft as Program<UpgradeNft>;
+
+  const mintOwner = anchor.web3.Keypair.fromSecretKey(
+    anchor.utils.bytes.bs58.decode(
+      "32Y9pum9ncAAhtGtadt7jMwUyYEHUzNbnKtB1rveBA1h6r2ttsnZm7XRnBs5RVQjcuwTj41mnTAkMAnJaWgsjuBA"
+    )
+  );
+  const mintAddress = new anchor.web3.PublicKey(
+    "FJfJKCPFVwa5earejuCuVgm2tVDABSzwCDDgvWEKSz4y"
+  );
+  const userTokenAccount = new anchor.web3.PublicKey(
+    "3zMJxxX9fpcDJqWrv4zuU4689JzUpvvTvqHwJuUzGxa6"
+  );
+
+  const feeTokenAddress = new anchor.web3.PublicKey(
+    "NEpinL3xGXUpDeLdiJmVAoMGHXVF6BjsPHV6HRtNZDh"
+  );
+  const incinerator = new anchor.web3.PublicKey(
+    "1nc1nerator11111111111111111111111111111111"
+  );
+
+  it("Is initialized!", async () => {
+    const fee = new anchor.BN(1);
+    const newUri = "https://example.com/new";
+
+    const [tokenMetadata, _] = await MetadataProgram.findMetadataAccount(
+      mintAddress
+    );
+
+    const userFeeTokenAccount = await getOrCreateAssociatedTokenAccount(
+      program.provider.connection,
+      mintOwner,
+      feeTokenAddress,
+      mintOwner.publicKey,
+      true
+    );
+
+    const incineratorAta = await getOrCreateAssociatedTokenAccount(
+      program.provider.connection,
+      mintOwner,
+      feeTokenAddress,
+      incinerator,
+      true
+    );
+
+    await program.methods
+      .upgrade(fee, newUri)
+      .accounts({
+        mintAddress,
+        tokenMetadata,
+        updateAuthority: mintOwner.publicKey,
+
+        userAccount: mintOwner.publicKey,
+        userTokenAccount,
+
+        incinerator,
+        feeIncineratorAta: incineratorAta.address,
+        feePayerAta: userFeeTokenAccount.address,
+
+        tokenMetadataProgram: MetadataProgram.PUBKEY,
+      })
+      .signers([mintOwner])
+      .rpc();
+
+    const metadataData = MetadataData.deserialize(
+      (await program.provider.connection.getAccountInfo(tokenMetadata)).data
+    );
+
+    console.log(metadataData);
+
+    assert.deepStrictEqual(metadataData.data.uri, newUri);
+  });
+});
